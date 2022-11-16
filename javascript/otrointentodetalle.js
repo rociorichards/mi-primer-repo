bc8da13999fcf083676e3c7cb4da5ac637f6ad11
aguus00 <floresa@udesa.edu.ar>
2022-11-16 11:19:46 -0300

    corrigiendo error

--- javascript/otrointentodetalle.js
+++ javascript/otrointentodetalle.js
@@ -1,10 +1,9 @@
 let query=location.search
-let api_key= "?api_key=8d82a1bf2b697b9dbc93552eab4d6d93&language=en-US"
 let endpoint= "https://api.themoviedb.org/3/movie/"
 let urlParams= new URLSearchParams(query)
 let id= urlParams.get('id')
 let titulo = document.querySelector(".titulo")
-fetch(`${endpoint}${id}${api_key}`)
+fetch(`${endpoint}${id}`)
 .then(function(respuesta){
     return respuesta.json()
 })
