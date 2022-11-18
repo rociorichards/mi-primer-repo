19f5ebb7a89af722af9c404b23114a47250367fc
aguus00 <floresa@udesa.edu.ar>
2022-11-18 10:43:02 -0300

    no puedo hacer pull

--- javascript/otrointentodetalle.js
+++ javascript/otrointentodetalle.js
@@ -1,6 +1,6 @@
 let query=location.search
-let api_key= "?api_key=8d82a1bf2b697b9dbc93552eab4d6d93"
-let urlParams= new URLSearchParams(query)
+let api_key= "8d82a1bf2b697b9dbc93552eab4d6d93"
+let objet= new URLSearchParams(query)
 let id= urlParams.get('id')
 let titulo = document.querySelector(".titulo")
 fetch(`https://api.themoviedb.org/3/movie/${id}`)
